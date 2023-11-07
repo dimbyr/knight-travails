873f3637bca75c1d6d8b414e8b5c7efcee3a32d6
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-11-07 05:16:36 +0200

    Built a Knight node

--- lib/vertex.rb
+++ lib/vertex.rb
@@ -1,17 +1,43 @@
 # Vertex of a graph
+class Node
+  attr_accessor :data, :neighbors, :predecessor, :distance, :children
 
-class Vertex
-  attr_accessor :data, :predecessor, :distance, :children
-
-  def initialize(data = nil, children = [], predecessor = nil, distance = nil)
+  def initialize(data = nil)
     @data = data
-    @children = children
-    @predecessor = predecessor
-    distance = distance
+    @neighbors = []
+    @children = []
+    @predecessor = nil
+    @distance = nil
   end
 
   def visited?
     !distance.nil?
   end
-
 end
+
+# A knight node
+class KnightNode < Node
+  def initialize(data = nil)
+    super
+    @neighbors = get_neighbors(data)
+  end
+
+  def get_neighbors(data)
+    return if data.nil?
+
+    i, j = data
+    where_to = [[2, 1], [2, -1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [1, -2], [-1, -2]]
+    neighbors = []
+    where_to.each do |a, b|
+      x = i + a
+      y = j + b
+      neighbors << [x, y] if x.between?(0, 7) && y.between?(0, 7)
+    end
+    neighbors
+  end
+end
+
+# pos = [7, 7]
+# knight = KnightNode.new(pos)
+# puts "#{pos} has #{knight.neighbors.length} neighbors"
+# p knight.neighbors
